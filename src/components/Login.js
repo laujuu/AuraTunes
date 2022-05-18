d4c28bbd432de5e9433f14f1076802cc9295445d
laujuu <lauroindcontato@gmail.com>
2022-05-18 15:49:15 -0300

    req 02 - Adiciona pagina de login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,71 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
-  render() {
-    return (
-      <main>
-        <div data-testid="page-login" />
-      </main>
-    );
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      isButtonDisabled: true,
+      login: false,
+    };
+    this.onInputChange = this.onInputChange.bind(this);
   }
+
+  onInputChange(event) {
+    const validation = 3;
+    const { name, value } = event.target;
+    if (value.length >= validation) {
+      this.setState({ isButtonDisabled: false, [name]: value });
+    }
+  }
+
+       onButtonClick = async () => {
+         const { name } = this.state;
+         this.setState({ loading: true });
+         await createUser({ name });
+         this.setState({ login: true, loading: false });
+       }
+
+       render() {
+         const {
+           isButtonDisabled,
+           loading,
+           login,
+         } = this.state;
+
+         return (
+           <main>
+             { loading && <Loading /> }
+             { login && <Redirect to="/search" /> }
+             <div data-testid="page-login">
+               <form className="main-left">
+                 <label htmlFor="name">
+                   Nome
+                   <br />
+                   <input
+                     type="text"
+                     name="name"
+                     onChange={ this.onInputChange }
+                     data-testid="login-name-input"
+                   />
+                 </label>
+
+                 <button
+                   type="button"
+                   disabled={ isButtonDisabled }
+                   onClick={ this.onButtonClick }
+                   data-testid="login-submit-button"
+                 >
+                   Entrar
+                 </button>
+               </form>
+             </div>
+           </main>
+         );
+       }
 }
 
 export default Login;
